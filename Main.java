d78e8ebea6fa87112d9712a41a9036009e11ea80
Ermolenko Danil <danil-ermolenko-01@mail.ru>
2023-06-29 21:13:29 +0300

    a lot of lines can be managed

--- Main.java
+++ Main.java
@@ -1,69 +1,62 @@
 package Calculator;
 
 import java.io.*;
-import java.util.Scanner;
-
+import java.io.BufferedReader;
 
 
 public class Main {
     public static  void main(String[] args) {
-        byte[] buffer = new byte[256];
-        String s = "";
-        try (FileInputStream fis = new FileInputStream("/C:/Users/zer0c/Desktop/Butusov/input.txt");
-                BufferedInputStream bis = new BufferedInputStream(fis);
-                FileOutputStream fos = new FileOutputStream("/C:/Users/zer0c/Desktop/Butusov/output.txt");
-                BufferedOutputStream bos = new BufferedOutputStream(fos) ) {
-            int count;
-                    while ((count = bis.read()) != -1) {
-                        s += (char) count;
-                    }
-            String[] str = s.split(" ");
-            double a = 0, b = 0;
-            boolean flag = true;
-            try {
-                a = Double.parseDouble(str[0]);
-                b = Double.parseDouble(str[2]);
-            } catch (NumberFormatException numberFormatException) {
-                flag = false;
-                s += " = Error! Not number";
-                System.out.println("Error! Not number");
-            }
-            if (flag) {
-                if ("+-*/".contains(str[1])) {
-                    switch (str[1]) {
-                        case "+":
-                            s += " = " + (a + b);
-                            break;
-                        case "-":
-                            s += " = " + (a - b);
-                            break;
-                        case "*":
-                            s += " = " + a * b;
-                            break;
-                        case "/":
-                            try {
-                                if (b != 0) {
-                                    s += a / b;
-                                } else {
-                                    s += " = Error! Division by zero";
-                                }
-                            } catch (ArithmeticException ae) {
-                                System.out.println("Error! Division by zero");
-                                s += " = Error! Not number";
-                            }
 
-                    }
-                } else {
-                    s += " = Operation error!";
+        String line;
+        try (FileReader fw = new FileReader("/C:/Users/zer0c/Desktop/Butusov/input.txt");
+                BufferedReader br = new BufferedReader(fw);
+                BufferedWriter bw = new BufferedWriter(new FileWriter("/C:/Users/zer0c/Desktop/Butusov/output.txt")))
+                {
+                while ((line = br.readLine()) != null) {
+                String[] str = line.split(" ");
+                double a = 0, b = 0;
+                boolean flag = true;
+                try {
+                    a = Double.parseDouble(str[0]);
+                    b = Double.parseDouble(str[2]);
+                } catch (NumberFormatException numberFormatException) {
+                    flag = false;
+                    line += " = Error! Not number";
+                    System.out.println("Error! Not number");
                 }
+                if (flag) {
+                    if ("+-*/".contains(str[1])) {
+                        switch (str[1]) {
+                            case "+":
+                                line += " = " + (a + b);
+                                break;
+                            case "-":
+                                line += " = " + (a - b);
+                                break;
+                            case "*":
+                                line += " = " + a * b;
+                                break;
+                            case "/":
+                                if (b != 0) {
+                                    line += " = " + a / b;
+                                } else {
+                                    line += " = Error! Division by zero";
+                                }
+                                break;
+                        }
+                    } else {
+                        line += " = Operation error!";
+                    }
+                }
+                bw.write(line + "\n");
+
             }
-            buffer = s.getBytes();
-            bos.write(buffer, 0, buffer.length);
-        } catch (FileNotFoundException e) {
-            System.out.println("File was not found");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            } catch(FileNotFoundException e){
+                System.out.println("File was not found");
+            } catch(IOException e){
+                throw new RuntimeException(e);
+            }
+
     }
 }
 
